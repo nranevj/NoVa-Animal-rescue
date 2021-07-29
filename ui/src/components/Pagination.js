2de93a5b7d0a87297b2c8dd47560575f18a2f430
Nitish Rane <rane.nitish@gmail.com>
2021-07-28 20:49:53 -0700

    Add pagination component and background image

--- ui/src/components/Pagination.js
+++ ui/src/components/Pagination.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import PaginationMaterialUI from '@material-ui/lab/Pagination';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiPaginationItem-root': {
+            width: "3vw",
+            borderRadius: "5vw",
+            height: '3vw'
+         },
+        '& .Mui-selected': {
+            width: "4vw",
+            borderRadius: "5vw",
+            height: '4vw'
+         }
+    }
+}))
 
 export function Pagination ({ postsPerPage, totalPosts, paginate }){
+    const classes = useStyles();
     const PageNumbers = [];
     
     for(let i=1; i <= Math.ceil(totalPosts/postsPerPage); i++){
@@ -11,7 +28,7 @@ export function Pagination ({ postsPerPage, totalPosts, paginate }){
     return (
         <nav>
             <PaginationMaterialUI count={Math.ceil(totalPosts/postsPerPage)} color="primary" 
-            onChange={(event, value) => paginate(value)} />
+            onChange={(event, value) => paginate(value)} className={classes.root}/>
         </nav>
     )
 }
