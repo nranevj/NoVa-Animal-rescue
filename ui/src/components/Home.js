b7c85a4edc5206578d2ed98fec1ffb736616e159
Nitish Rane <rane.nitish@gmail.com>
2021-08-03 21:57:03 -0700

    Clean code

--- ui/src/components/Home.js
+++ ui/src/components/Home.js
@@ -1,26 +1,21 @@
 import React, { useState }  from 'react';
 import { SignIn } from './SignIn';
 import { SignUp } from './SignUp';
-import { Feed } from "./Feed";
 import { Feed1 } from "./Feed1";
 import "../css/Home.css";
 
 export function Home() {
-    const [loggedin, setLogin] = useState(false);
+    const [loggedin, setLogin] = useState(true);
+    const [signedup, setSignedUp] = useState(true);
 
     return (
         <>
-            <Feed1 />
-
-            {/* TODO: routing */}
-
-            {/* { loggedin ? <Feed /> : 
+            { loggedin ? <Feed1 /> : 
                 <div class="b-split-screen">
                     <div class="left-pane"></div>
-                    <div class="right-pane">{ loggedin ? <SignIn /> : <SignUp /> }</div>
+                    <div class="right-pane">{ signedup ? <SignIn/> : <SignUp/> }</div>
                 </div>
-            } */}
-            
+            }
         </>
     );
     
